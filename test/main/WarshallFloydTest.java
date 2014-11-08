2468bac5417276f1dee76de8a07b937bda0f3329
lucp <l.potepa@gmail.com>
2014-11-08 23:58:40 +0100

    WarshallFloyd working

--- test/main/WarshallFloydTest.java
+++ test/main/WarshallFloydTest.java
@@ -1,13 +1,10 @@
 package main;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
 import factories.GraphIntegerDoubleFactory;
 import graphs.MatrixGraph;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import reader.GraphFileReader;
@@ -19,7 +16,7 @@ public class WarshallFloydTest {
 	public void executeTest() throws IOException {
 		GraphIntegerDoubleFactory factory = new GraphIntegerDoubleFactory();
 		GraphFileReader<Integer, Double> graphReader = new GraphFileReader<Integer, Double>(factory);
-		MatrixGraph<Integer, Double> matrixGraph = new MatrixGraph<Integer, Double>(graphReader.readGraphFile("res/graf.txt"));
+		MatrixGraph<Integer, Double> matrixGraph = new MatrixGraph<Integer, Double>(graphReader.readGraphFile("res/maly_graf.txt"));
 		WarshallFloyd<Integer, Double> warshallFloyd = new WarshallFloyd<Integer, Double>(matrixGraph);
 		Number[][] path = warshallFloyd.execute();
 		print(path);
@@ -28,7 +25,8 @@ public class WarshallFloydTest {
 	public void print(Number[][] path){
 		for (int i = 0; i < path.length; i++){
 			for (int j = 0; j < path.length; j++){
-				System.out.print(path[i][j]+"\t");
+				if (path[i][j].doubleValue() == Double.MAX_VALUE) System.out.print("Inf\t");
+				else System.out.print(path[i][j]+"\t");
 			}
 			System.out.println();
 		}
