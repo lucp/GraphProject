9dda161a1707e6529b0ca84a1f6d420122add4ee
lpot <lpot@ericpol.com>
2014-11-28 14:53:08 +0100

    FordFulkerson last refractor before testing

--- src/algorithms/FordFulkerson.java
+++ src/algorithms/FordFulkerson.java
@@ -4,50 +4,49 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 
-import factories.GraphObjectFactory;
 import graphs.Graph;
 import graphs.ListElement;
 
 public class FordFulkerson<VertexType, EdgeType extends Number & Comparable<EdgeType>> {
 
 	private Graph<VertexType, EdgeType> graph;
-	private HashMap<EdgeType, Number> flow;
-	private HashMap<EdgeType, Number> capacity;
 	private BFS<VertexType, EdgeType> bfs;
-	private HashSet<EdgeType> forbiddenEdges;
+	
 	
 	public FordFulkerson(Graph<VertexType, EdgeType> graph){
 		this.graph = graph;
-		this.capacity = new HashMap<EdgeType, Number>();
-		this.flow = new HashMap<EdgeType, Number>();
 		this.bfs = new BFS<VertexType, EdgeType>(this.graph);
-		this.forbiddenEdges = new HashSet<EdgeType>();
 	}
 	
 	public Number execute(VertexType source, VertexType destination){
+		HashMap<EdgeType, Number> flow = new HashMap<EdgeType, Number>();
+		HashMap<EdgeType, Number> capacity = new HashMap<EdgeType, Number>();
+		HashSet<EdgeType> forbiddenEdges = new HashSet<EdgeType>();
+		HashSet<EdgeType> inEdges = new HashSet<EdgeType>();
 		for (EdgeType edge : this.graph.getEdges()){
-			this.flow.put(edge, 0d);
-			this.capacity.put(edge, edge); //TODO check if not null with that
+			flow.put(edge, 0d);
+			capacity.put(edge, edge);
 		}
-		HashSet<VertexType> inVerticies = new HashSet<VertexType>();
-		LinkedList<ListElement<VertexType, EdgeType>> path = bfs.findPathAsListElements(source, destination, this.forbiddenEdges);
+		LinkedList<ListElement<VertexType, EdgeType>> path = bfs.findPathAsListElements(source, destination, forbiddenEdges);
 		while (path != null){
-			inVerticies.add(path.get(path.size() - 2).inVertex);
-			Number minEdge = this.capacity.get(path.getFirst().inEdge);
+			inEdges.add(path.getLast().inEdge);
+			Number minEdge = capacity.get(path.getLast().inEdge);
 			for (ListElement<VertexType, EdgeType> listElement : path){ //TODO check if null
-				if (minEdge.doubleValue() < this.capacity.get(listElement.inEdge).doubleValue()){
-					minEdge = this.capacity.get(listElement.inEdge).doubleValue();
+				if (listElement.inEdge != null){
+					if (minEdge.doubleValue() > capacity.get(listElement.inEdge).doubleValue()){
+						minEdge = capacity.get(listElement.inEdge).doubleValue();
+					}
 				}
 			}
 			for (ListElement<VertexType, EdgeType> listElement : path){
-				this.flow.replace(listElement.inEdge, this.flow.get(listElement.inEdge).doubleValue() + minEdge.doubleValue());
-				this.capacity.replace(listElement.inEdge, this.flow.get(listElement.inEdge).doubleValue() - minEdge.doubleValue());
-				if (this.capacity.get(listElement.inEdge).doubleValue() == 0d) this.forbiddenEdges.add(listElement.inEdge);
+				flow.put(listElement.inEdge, flow.get(listElement.inEdge).doubleValue() + minEdge.doubleValue());
+				capacity.put(listElement.inEdge, flow.get(listElement.inEdge).doubleValue() - minEdge.doubleValue());
+				if (capacity.get(listElement.inEdge).doubleValue() == 0d) forbiddenEdges.add(listElement.inEdge);
 			}
 		}
 		Number finalFlow = 0;
-		for (VertexType inVertex : inVerticies){
-			finalFlow = finalFlow.doubleValue() + this.graph.getEdge(inVertex, destination).doubleValue();
+		for (EdgeType inEdge : inEdges){
+			finalFlow = finalFlow.doubleValue() + flow.get(inEdge).doubleValue();
 		}
 		return finalFlow;
 	}
