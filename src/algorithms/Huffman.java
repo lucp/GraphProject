ae4a87a2c190a64e99fa77b231c961e083958282
lpot <lpot@ericpol.com>
2014-12-09 16:02:11 +0100

    Huffman to test

--- src/algorithms/Huffman.java
+++ src/algorithms/Huffman.java
@@ -3,10 +3,8 @@ package algorithms;
 import graphs.Graph;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 
-import model.GraphObjectFactory;
 import model.HEdge;
 import model.HVertex;
 
@@ -34,31 +32,40 @@ public class Huffman {
 	public Graph<HVertex, HEdge> generateTree() {
 		while (this.trees.size() > 1) {
 			Graph<HVertex, HEdge> minRoot1 = null;
+			HVertex minRootVertex1 = null;
 			int minRootProbability1 = Integer.MAX_VALUE;
 			Graph<HVertex, HEdge> minRoot2 = null;
+			HVertex minRootVertex2 = null;
 			int minRootProbability2 = Integer.MAX_VALUE;
 			for (Graph<HVertex, HEdge> tree : this.trees) {
-				int treeProbability = tree.getRoot().probability;
+				HVertex treeRoot = tree.getRoot();
+				int treeProbability = treeRoot.probability;
 				if (treeProbability < minRootProbability2) {
 					if (treeProbability < minRootProbability1) {
 						minRoot2 = minRoot1;
+						minRootVertex2 = minRootVertex1;
 						minRootProbability2 = minRootProbability1;
 						minRoot1 = tree;
+						minRootVertex1 = treeRoot;
 						minRootProbability1 = treeProbability;
 					}
 					else {
 						minRoot2 = tree;
+						minRootVertex2 = treeRoot;
 						minRootProbability2 = treeProbability;
 					}
 				}
 			}
+			minRoot1.mergeWith(minRoot2);
 			HVertex hvertex = new HVertex(minRootProbability1 + minRootProbability2, null);
 			HEdge hedge0 = new HEdge((byte)0);
 			HEdge hedge1 = new HEdge((byte)1);
 			minRoot1.addVertex(hvertex);
-//			minRoot1.addEdge(source, destination, edge);
+			minRoot1.addEdge(hvertex, minRootVertex1, hedge0);
+			minRoot1.addEdge(hvertex, minRootVertex2, hedge1);
+			this.trees.remove(minRoot2);
 		}
-		return null; //TODO
+		return this.trees.getFirst();
 	}
 	
 }
